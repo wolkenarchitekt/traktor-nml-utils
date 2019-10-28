f779e3d1253ca330886c918e2583f9036c09895a
Ingo Fischer <mail@ingofischer.de>
2019-10-28 23:10:11 +0100

    implement date parsing

--- traktor_nml_utils/main.py
+++ traktor_nml_utils/main.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass, field
 import xmltodict
 from lxml import etree
-
+from datetime import datetime
 
 @dataclass
 class TraktorCollectionEntry:
@@ -31,7 +31,33 @@ class TraktorPlaylistEntry:
       <EXTENDEDDATA DECK="1" DURATION="241.700394" EXTENDEDTYPE="HistoryData" PLAYEDPUBLIC="1" STARTDATE="132319762" STARTTIME="68839"/>
     </ENTRY>
     """
-    xml_dict: 'TraktorCollection' = field(repr=False)
+
+    """
+    Date trans:
+    2017-12-31: 132189215
+    2018-01-01: 132251905 +62690
+    2018-12-31: 132254751
+    2019-01-01: 132317441 +62690
+    2019-01-02: 132317442
+    2019-01-03: 132317443
+    2019-01-31: 132317471
+    2019-02-01: 132317697 +226
+    2019-02-28: 132317724 +27
+    2019-03-01: 132317953 +229
+    2019-03-31: 132317983 +30
+    2019-04-01: 132318209 +226
+    2019-04-30: 132318238
+    2019-05-01: 132318465 +227
+    2019-05-31: 132318495
+    2019-06-01: 132318721 +226
+    
+    https://github.com/drumsoft/TraktorLogDecode/blob/master/traktor-log-decode.pl
+    sprintf "%04d-%02d-%02d", 
+		$trday>>16, ($trday>>8 & 0xff), ($trday & 0xff);
+    """
+    duration: float
+    filename: str
+    startdatetime: datetime
 
 
 class TraktorCollection:
@@ -60,8 +86,39 @@ class TraktorCollection:
     def playlists(self) -> [TraktorPlaylistEntry]:
         if not self._playlist_entries:
             # /PLAYLISTS/NODE[@TYPE='FOLDER']/SUBNODES/NODE[@TYPE='PLAYLIST'][@NAME='Auto-Cued Tracks']/PLAYLIST/ENTRY
-            self._playlist_entries = self.xml_tree.findall(
-                "/PLAYLISTS/NODE[@TYPE='FOLDER']/SUBNODES/NODE[@TYPE='PLAYLIST'][@NAME='HISTORY']/PLAYLIST/ENTRY")
+            for entry in self.xml_tree.findall(
+                    "/PLAYLISTS/NODE[@TYPE='FOLDER']/SUBNODES/NODE[@TYPE='PLAYLIST'][@NAME='HISTORY']/PLAYLIST/ENTRY"):
+                duration = float(entry.xpath('EXTENDEDDATA/@DURATION')[0])
+                filename = entry.xpath('PRIMARYKEY/@KEY')[0]
+
+                startdate = entry.xpath('EXTENDEDDATA/@STARTDATE')[0]
+                startdate = int(startdate)
+                starttime = entry.xpath('EXTENDEDDATA/@STARTTIME')[0]
+                starttime = int(starttime)
+                # sprintf "%02d:%02d:%02d", int($s / 3600), int($s / 60) % 60, ($s % 60);
+
+                year = startdate >> 16
+                month = startdate >> 8 & 0xff
+                day = startdate & 0xff
+                hour = int(starttime / 3600)
+                minute = int(starttime / 60) % 60
+                second = starttime % 60
+                startdate = datetime(year=year, month=month, day=day,
+                                     hour=hour, minute=minute, second=second)
+
+                playlist_entry = TraktorPlaylistEntry(
+                    duration=duration, filename=filename, startdatetime=startdate)
+                self._playlist_entries.append(playlist_entry)
+
+                print(playlist_entry.filename, playlist_entry.startdatetime.isoformat())
+
+                # from ipdb import set_trace
+                # set_trace()
+                # pass
+            # self._playlist_entries = self.xml_tree.findall(
+            #     "/PLAYLISTS/NODE[@TYPE='FOLDER']/SUBNODES/NODE[@TYPE='PLAYLIST'][@NAME='HISTORY']/PLAYLIST/ENTRY")
+            # from ipdb import set_trace
+            # set_trace()
 
         return self._playlist_entries
 
