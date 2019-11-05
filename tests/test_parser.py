638c5a7c91d5093fa90d87c41498569db3b4d8b0
Ingo Fischer <mail@ingofischer.de>
2019-11-05 18:55:31 +0100

    Fix travis build

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -75,7 +75,3 @@ def test_history(nml_file):
     assert history_entry.starttime == 68839
     assert history_entry.filename == \
         'Transcend/:DJing/:beatport_steve/:09 Change (Like Mix Extended Instrumental).mp3'
-
-
-def test_travis():
-    assert False
