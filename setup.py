50a2241ea881bd1c4ef9b2b2882dfdfce6e563ec
Ingo Fischer <mail@ingofischer.de>
2020-08-31 21:41:56 +0200

    Bump version: 2.0.2 → 2.0.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='traktor-nml-utils',
-    version='2.0.2',
+    version='2.0.3',
     description='Utilities to read and write Traktor NML files',
     long_description=read('README.pypi.md'),
     long_description_content_type='text/markdown',
