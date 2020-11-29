5754ee22bd40cb768110c984ba9b55ca7f87dc12
Ingo Fischer <mail@ingofischer.de>
2020-11-29 19:16:14 +0100

    Simplify import CLI - import directories recursively

--- traktor_nml_utils/cli.py
+++ traktor_nml_utils/cli.py
@@ -1,25 +1,18 @@
-from os.path import isfile
-from pathlib import Path
 import logging
 import os
 import sys
+from pathlib import Path
 
 import click
-from traktor_nml_utils import TraktorCollection
+
+from traktor_nml_utils import TraktorCollection, is_history_file, TraktorHistory
 
 logger = logging.getLogger(__name__)
 
 
-def get_nml_files(directory):
-    return [
-        Path(file) for file in os.scandir(directory) if
-        isfile(file) and file.name.endswith('.nml')
-    ]
-
-
 @click.group()
-@click.option('-v', '--verbose', count=True, help="Verbose logging")
-@click.option('-d', '--debug', help="Debug logging")
+@click.option("-v", "--verbose", count=True, help="Verbose logging")
+@click.option("-d", "--debug", help="Debug logging")
 def cli(verbose, debug):
     loglevel = logging.WARNING
     if verbose:
@@ -30,23 +23,23 @@ def cli(verbose, debug):
 
 
 @cli.command()
-@click.argument('nml_file')
-def traktor_import_file(nml_file):
-    """NML file to import."""
-    if not os.path.exists(nml_file):
-        sys.exit(f"NML file {nml_file} does not exist")
-    TraktorCollection(Path(nml_file))
+@click.argument("nml")
+def traktor_import(nml):
+    """NML import from file or directory."""
+    p = Path(nml)
+    if p.is_dir():
+        nml_files = p.glob('**/*.nml')
+    else:
+        nml_files = [p]
+
+    for nml_file in nml_files:
+        print(f"Importing NML: {nml_file}")
+        if is_history_file(nml_file):
+            TraktorHistory(nml_file)
+        else:
+            TraktorCollection(nml_file)
 
 
-@cli.command()
-@click.argument('nml_dir')
-def traktor_import_dir(nml_dir):
-    """NML directory to import."""
-    for nml_file in get_nml_files(nml_dir):
-        collection = TraktorCollection(Path(nml_file))
-        for entry in collection.entries:
-            print(f"{nml_file} {entry.artist} - {entry.title}")
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
+
